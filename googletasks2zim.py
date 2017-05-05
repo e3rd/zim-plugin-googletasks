72db061b7afa36eb0fe93b7c407723983b735371
Edvard Rejthar <edvard.rejthar@nic.cz>
2017-05-05 16:38:22 +0200

    working version

--- googletasks2zim.py
+++ googletasks2zim.py
@@ -13,7 +13,7 @@ from zim.plugins import extends
 from zim.config import XDG_DATA_HOME
 from zim.notebook import Path
 from zim.main import NotebookCommand
-#from zim.command import Command
+from zim.ipc import start_server_if_not_running, ServerProxy
 
 from apiclient import discovery
 from oauth2client import client
@@ -23,6 +23,7 @@ from oauth2client.file import Storage
 import datetime
 
 WORKDIR = str(XDG_DATA_HOME.subdir(('zim', 'plugins')))
+CACHEFILE = WORKDIR + "/googletasks2zim.cache"
 
 logger = logging.getLogger('zim.plugins.instantsearch')
 class Googletasks2zimPlugin(PluginClass):
@@ -35,29 +36,21 @@ Connects to your default Google Tasks lists. Append today's tasks to your Home f
 '''),
         'author': "Edvard Rejthar"        
     }
-        
-    #def __init__(self, config=None):
-        #PluginClass.__init__(self, config)
-        #self.preferences.connect('changed', self.on_preferences_changed)
-        #self.on_preferences_changed(self.preferences)
-        #klass = self.get_extension_class()
-        #self.set_extension_class('MainWindow', klass)
-        #print("HEEEEEEJ")
-        #GoogletasksWindow().fetch(window = None)
-    #    pass
-
-
+            
 
 class GoogletasksCommand(NotebookCommand):
     '''Class to handle "zim --plugin trayicon" '''    
     arguments = ('[NOTEBOOK]',)
 
-    def run(self):
-            #print("HEJ", self.arguments[0])
-            import ipdb; ipdb.set_trace()
-            notebook, _ = self.build_notebook()                                    
-            Googletasks().fetch(notebook = notebook)
-
+    def run(self):            
+            start_server_if_not_running()
+            server = ServerProxy()            
+            ui = server.get_notebook(self.get_notebook_argument()[0].uri, False)
+            reload = (lambda: ui.reload_page()) if ui else lambda: None
+            ntb, _ = self.build_notebook()
+            reload() # save current user's work, if zim's open
+            Googletasks().fetch(notebook = ntb) # add new lines
+            reload() # save new lines            
 
 @extends('MainWindow')
 class GoogletasksWindow(WindowExtension):
@@ -83,82 +76,92 @@ class GoogletasksWindow(WindowExtension):
         #import ipdb; ipdb.set_trace()
         Googletasks().fetch(window = self.window)
         
-    
+import pickle    
+import dateutil.parser
+
 class Googletasks(object):    
-    def fetch(self, window = None, notebook = None):
+ 
+    def _get_new_items(self):        
         credentials = self.get_credentials()
         http = credentials.authorize(httplib2.Http())
         service = discovery.build('tasks', 'v1', http=http)
+        now = datetime.datetime.utcnow()
+        today = now.isoformat()[:11]
         
-        now = datetime.datetime.utcnow().isoformat()[:11]     
+        if os.path.isfile(CACHEFILE):
+            with open(CACHEFILE, "rb") as f:
+                self.recentItemIds = pickle.load(f)                
+            dueMin = datetime.datetime.fromtimestamp(os.path.getmtime(CACHEFILE)).isoformat()[:11]
+        else:
+            self.recentItemIds = set()
+            dueMin = today
+        
+        
         results = service.tasks().list(maxResults=10, 
                                     tasklist = "@default",
                                     showCompleted = False,
-                                    dueMax = now + "23:59:59.999Z",
-                                    dueMin = now + "00:00:00.000Z").execute()
+                                    dueMax = today + "23:59:59.999Z",
+                                    dueMin = dueMin + "00:00:00.000Z").execute()
         items = results.get('items', [])
         if not items:
-            logger.info('No task lists found.')
+            logger.info('********** No task lists found.')
             return
         else:
             text = ""
-            logger.info('Task lists added.')
+            logger.info('**********  Task lists added.')            
             for item in items:
+                if item["id"] in self.recentItemIds:
+                    if dateutil.parser.parse(item["due"]).date() >= now.date():
+                        self.itemIds.add(item['id'])                        
+                    logger.debug('Skipping {}.'.format(item['title']))
+                    #import ipdb; ipdb.set_trace()
+                    continue
+                self.itemIds.add(item['id'])
                 text += '[ ] {0}{1}\n'.format(item['title'], ("\n\t" + item['notes']) if "notes" in item else "")                    
-            text += "\n"
+            if text.strip() != "":
+                text += "\n"
+        return text
+    
+    def fetch(self, window = None, notebook = None):
+        self.itemIds = set()
+        
+        
+        text = self._get_new_items()
             
         if not notebook and window:
             notebook = window.ui.notebook
             
         # Insert tasks string into page        
-        hp = notebook.get_page(Path("holuba")) #notebook.get_home_page()
-        if window and window.pageview.get_page().name is hp.name: # HP is current page - we use GTK buffers, caret stays at position            
-            buffer = window.pageview.view.get_buffer()                        
-            #bounds = buffer.get_selection_bounds()
-            #if not bounds:
-                #i = buffer.get_insert_iter().get_offset()
-                #bounds = (i,i,)                        
-            #buffer.select_range(buffer.get_iter_at_offset(100),buffer.get_iter_at_offset(200))
-            
-            counter = 1
-            offset = 0
-            for i in range(buffer.get_line_count()):            
-                start, end = buffer.get_line_bounds(i)
-                line = buffer.get_text(start, end)
-                if line.strip() == "":
-                        counter -= 1
-                if counter == 0:
-                    offset = end
-                    break            
-            
-            if offset == 0:            
-                offset = buffer.get_end_iter()        
-                text = "\n" + text        
-            cursor = buffer.get_iter_at_offset(offset.get_offset())        
-            buffer.insert(cursor, text)
-        else: # HP is not current page - we change the file directly
-            counter = 1
-            contents = []            
-            for line in hp.dump("wiki"): #hp.source.readlines():
-                contents.append(line)
-                if line.strip() == "":
-                    counter -= 1
-                if counter == 0:
-                    contents.append(text)
-                    counter = -1        
-            hp.parse('wiki', "".join(contents))
-            #hp.source.writelines(contents)            
+        hp = notebook.get_page(Path("holuba")) # XXX notebook.get_home_page()        
+                    
         #import ipdb; ipdb.set_trace()
-            #reload_page
-            #hp.parse("wiki"
-            #page.parse('wiki', text
+        counter = 1
+        contents = []            
+        for line in hp.dump("wiki"):
+            contents.append(line)
+            if line.strip() == "":
+                counter -= 1
+            if counter == 0:
+                contents.append(text)
+                counter = -1        
+        bounds = None
+        if window and window.pageview.get_page().name is hp.name:
+            # HP is current page - we use GTK buffers, caret stays at position            
+            buffer = window.pageview.view.get_buffer()                        
+            bounds = [x.get_offset() for x in buffer.get_selection_bounds()]
+            if not bounds:
+                i = buffer.get_insert_iter()
+                bounds = [i.get_offset()] * 2
+            
+        hp.parse('wiki', "".join(contents))
         notebook.store_page(hp)
-        print("STORING PAGE")
-        #hp.set_ui_object(None) # refresh zim cache
-        #hp.set_parsetree(None)
-        return
-
-
+        if bounds:            
+            buffer.select_range(buffer.get_iter_at_offset(bounds[0]), buffer.get_iter_at_offset(bounds[1]))
+            
+            
+        # Save the success info
+        with open(CACHEFILE, "wb") as f:
+            pickle.dump(self.itemIds, f)            
 
     def get_credentials(cls):
         """Gets valid user credentials from storage.
@@ -171,19 +174,17 @@ class Googletasks(object):
         """                
         SCOPES = 'https://www.googleapis.com/auth/tasks.readonly' # If modifying these scopes, delete your previously saved credentials
         CLIENT_SECRET_FILE = os.path.join(WORKDIR, 'googletasks2zim_client_id.json')
-        APPLICATION_NAME = 'googletasks2zim'  
-        #import ipdb; ipdb.set_trace()        
+        APPLICATION_NAME = 'googletasks2zim'
 
-        if not os.path.isfile(CLIENT_SECRET_FILE):            
-            quit(Googletasks2zimPlugin.plugin_info["name"] + "CLIENT_SECRET_FILE not found")            
-                    
+        if not os.path.isfile(CLIENT_SECRET_FILE):
+            quit(Googletasks2zimPlugin.plugin_info["name"] + "CLIENT_SECRET_FILE not found")
             
         credential_path = os.path.join(WORKDIR, 'googletasks2zim.json')    
 
         store = Storage(credential_path)
         credentials = store.get()
-        if not credentials or credentials.invalid:            
-            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)            
+        if not credentials or credentials.invalid:
+            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
             flow.user_agent = APPLICATION_NAME # Googletasks2zimPlugin.plugin_info["name"]                         
             argv = sys.argv 
             sys.argv = sys.argv[:1] # tools.run_flow unfortunately parses arguments and would die from any zim args
